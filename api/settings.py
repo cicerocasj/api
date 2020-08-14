fcd7bbf0fa7d0bc546d4bc7073e02dcc25587c3e
cicerocasj@gmail.com <cicerocasj@gmail.com>
2020-08-14 12:12:42 -0300

    remove debug mode

--- api/settings.py
+++ api/settings.py
@@ -5,13 +5,45 @@ import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY', 'XXXXX')
+DEBUG = False
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '.herokuapp.com', 'cicerocasj-api.herokuapp.com']
 
-with open('application_log_config.json') as json_file:
-    LOGGING = json.load(json_file)
-LOGGING.get('handlers').get('applogfile')['filename'] = 'application.log'
+# with open('application_log_config.json') as json_file:
+#     LOGGING = json.load(json_file)
+# LOGGING.get('handlers').get('applogfile')['filename'] = 'application.log'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -43,6 +75,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ROOT_URLCONF = 'api.urls'
 
